56bbfb5ccc5d347ee4b625b1fa69b82816ad4191
Benjamin O <jeep70cp@gmail.com>
2019-09-16 22:54:54 -0400

    try catch

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import * as exec from '@actions/exec';
 import * as toolCache from '@actions/tool-cache';
 
 async function run() {
-  let installer = await toolCache.downloadTool("http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe");
-  let installer_script = toolCache.downloadTool("https://raw.githubusercontent.com/jurplel/install-qt-action/master/qt-installer-noninteractive.qs");
-  await exec.exec(`"${installer}"`, ['--verbose', `'--script ${installer_script}'`])
+  try {
+    let installer = await toolCache.downloadTool("http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe");
+    let installer_script = toolCache.downloadTool("https://raw.githubusercontent.com/jurplel/install-qt-action/master/qt-installer-noninteractive.qs");
+    await exec.exec(`"${installer}"`, ['--verbose', `'--script ${installer_script}'`])
+  
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
