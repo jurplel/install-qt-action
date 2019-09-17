d04c400712a21ca97816710e4919a69cc403b7af
Benjamin O <jeep70cp@gmail.com>
2019-09-16 22:56:30 -0400

    forgot to await that

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as toolCache from '@actions/tool-cache';
 async function run() {
   try {
     let installer = await toolCache.downloadTool("http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe");
-    let installer_script = toolCache.downloadTool("https://raw.githubusercontent.com/jurplel/install-qt-action/master/qt-installer-noninteractive.qs");
+    let installer_script = await toolCache.downloadTool("https://raw.githubusercontent.com/jurplel/install-qt-action/master/qt-installer-noninteractive.qs");
     await exec.exec(`"${installer}"`, ['--verbose', `'--script ${installer_script}'`])
   
   } catch (error) {
