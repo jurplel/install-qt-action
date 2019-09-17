3b1f2363b0275cc5451b8da3c6130fa47daada3f
Benjamin O <jeep70cp@gmail.com>
2019-09-16 23:37:34 -0400

    Ok probably fixed paths?

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+import * as fs from "fs";
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as toolCache from '@actions/tool-cache';
 
 async function run() {
   try {
-    let installer = await toolCache.downloadTool("http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe");
-    let installer_script = await toolCache.downloadTool("https://raw.githubusercontent.com/jurplel/install-qt-action/master/qt-installer-noninteractive.qs");
-    await exec.exec(`"${installer}"`, ['--verbose', `'--script ${installer_script}'`])
+    const initialInstallerPath = await toolCache.downloadTool("http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe");
+    const initialInstallerScriptPath = await toolCache.downloadTool("https://raw.githubusercontent.com/jurplel/install-qt-action/master/qt-installer-noninteractive.qs");
+    
+    const installerPath = initialInstallerPath + ".exe";
+    fs.renameSync(initialInstallerPath, installerPath);
+
+    const installerScriptPath = initialInstallerScriptPath + ".qs";
+    fs.renameSync(initialInstallerScriptPath, installerScriptPath);
+    
+    await exec.exec(`"${installerPath}"`, ['--verbose', `--script "${installerScriptPath}"`])
   
   } catch (error) {
     core.setFailed(error.message);
