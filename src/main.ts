36085ea0d0fac17915a5277f28913f27b145a580
Benjamin O <jeep70cp@gmail.com>
2019-09-16 22:32:24 -0400

    Getting started

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import * as core from '@actions/core';
+import * as toolCache from '@actions/tool-cache';
 
 async function run() {
-  try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  let installer = await toolCache.downloadTool("http://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe");
 }
 
 run();
