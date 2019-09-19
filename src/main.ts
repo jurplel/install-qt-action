3ebf533c90358b41db586f225759d1180e90a4eb
Benjamin O <jeep70cp@gmail.com>
2019-09-18 23:11:08 -0400

    windows toolkit

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as toolCache from '@actions/tool-cache';
 async function run() {
   try {
     await exec.exec("pip install aqtinstall")
-    await exec.exec("python -m aqt install -O " + process.env.GITHUB_WORKSPACE + " 5.12.5 windows desktop")
+    await exec.exec("python -m aqt install -O " + process.env.GITHUB_WORKSPACE + " 5.12.5 windows desktop win64_msvc2017_64")
 
   } catch (error) {
     core.setFailed(error.message);
