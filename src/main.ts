37780ebe6c2e844c8641d8235cfdc496eed687b6
Benjamin O <jeep70cp@gmail.com>
2019-09-18 23:08:54 -0400

    use python -m command

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as toolCache from '@actions/tool-cache';
 async function run() {
   try {
     await exec.exec("pip install aqtinstall")
-    await exec.exec("aqt install -O " + process.env.GITHUB_WORKSPACE + " 5.12.5 windows desktop")
+    await exec.exec("python -m aqt install -O " + process.env.GITHUB_WORKSPACE + " 5.12.5 windows desktop")
 
   } catch (error) {
     core.setFailed(error.message);
