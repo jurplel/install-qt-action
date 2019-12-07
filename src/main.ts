a232f100b3b481edf0b88dcf1a563ed16faaecb7
Benjamin O <jeep70cp@gmail.com>
2019-12-06 20:33:57 -0500

    removed --pre

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function run() {
     }
 
     await exec.exec("pip3 install setuptools wheel");
-    await exec.exec("pip3 install \"aqtinstall==0.6.*\" --pre");
+    await exec.exec("pip3 install \"aqtinstall==0.6.*\"");
 
     const dir = (core.getInput("dir") || process.env.RUNNER_WORKSPACE) + "/Qt";
     const version = core.getInput("version");
