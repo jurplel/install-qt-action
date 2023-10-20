f0fce889ff46499126b92502b5523793922b76d0
Benjamin O <jurplel@interversehq.com>
2023-10-20 00:24:57 -0400

    Revert "Revert "Add Windows ARM support""

--- action/src/main.ts
+++ action/src/main.ts
@@ -41,11 +41,11 @@ const locateQtArchDir = (installDir: string): string => {
     .sync(`${installDir}/[0-9]*/*/bin/qmake*`)
     .map((s) => s.replace(/\/bin\/qmake[^/]*$/, ""));
 
-  // For Qt6 mobile and wasm installations, a standard desktop Qt installation
-  // must exist alongside the requested architecture.
-  // In these cases, we must select the first item that ends with 'android*', 'ios', or 'wasm*'.
+  // For Qt6 mobile and wasm installations, and Qt6 Windows on ARM installations,
+  // a standard desktop Qt installation must exist alongside the requested architecture.
+  // In these cases, we must select the first item that ends with 'android*', 'ios', 'wasm*' or 'msvc*_arm64'.
   const requiresParallelDesktop = qtArchDirs.filter((p) =>
-    p.match(/6\.\d+\.\d+\/(android[^/]*|ios|wasm[^/]*)$/)
+    p.match(/6\.\d+\.\d+\/(android[^/]*|ios|wasm[^/]*|msvc[^/]*_arm64)$/)
   );
   if (requiresParallelDesktop.length) {
     // NOTE: if multiple mobile/wasm installations coexist, this may not select the desired directory
